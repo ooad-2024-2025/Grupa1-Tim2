aa42271a85b68adc0c8fbe9f54f96f084c2d77d4
irmal <irmalemes09@gmail.com>
2025-04-15 09:49:11 +0200

    inital commit

---
+++ YourRide/YourRide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YourRide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
